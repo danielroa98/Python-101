935f837becaa02ddc86f363fb940ef3be242348d
Daniel Roa <daniel.roa98@gmail.com>
2023-08-23 20:18:14 -0600

    Update script so that it contains further examples.

---
+++ 01-BasicDataTypes.py
@@ -0,0 +1,60 @@
+# 01-BasicDataTypes.py
+# Created by Daniel Roa on July 19, 2023
+
+# INTS
+# Ejemplos básicos de enteros
+print(19)
+print(2019)
+print(-397)
+print(-90)
+
+## Operaciones
+print(1+3)
+print(54+24*12-(4**2))
+print(24242/12)
+
+# FLOATS
+# Ejemplos básicos con floats
+print("Valores floats")
+print(192.3)
+print(42.0021)
+print(671.102)
+print(-192.01)
+
+# Operaciones
+print("Resultado de operaciones con floats")
+print("54*12.32 =", 54*12.32)
+print("65+123*0.612-(23.05**2) =", 65+123*0.612-(23.05**2))
+print("89.653/2.43 = ", 89.653/2.43)
+
+# STRINGS
+# Ejemplos básicos con Strings
+print("Strings")
+print("Hello, World!")
+print('Python! Woooo!')
+print("This is a basic Python example.")
+print('She said, "Hello!"')
+
+# Operaciones
+print("Operaciones con Strings")
+print("Hello, " + "World!")               # Concatenación
+print("Python " * 3)                      # Repetición
+print("Hello!"[0])                        # Indexación - imprimirá 'H'
+print("World"[-1])                        # Indexación - imprimirá 'd'
+print("Python".upper())                   # Convertir a mayúsculas
+print("PYTHON".lower())                   # Convertir a minúsculas
+
+# BOOLEANS
+# Ejemplos básicos de Booleans
+print(True)
+print(False)
+
+# Ejemplos básicos (y generales) con Booleans
+print("True and False = ", True and False)  # AND
+print("True or False = ", True or False)    # OR
+print("not True", not True)                 # NOT
+print("5 > 3", 5 > 3)                       # Greater than
+print("5 < 3", 5 < 3)                       # Less than
+print("5 == 5", 5 == 5)                     # Equality
+print("5 != 5", 5 != 5)                     # Inequality
+
