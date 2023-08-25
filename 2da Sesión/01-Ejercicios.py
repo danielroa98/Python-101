a71d91a4cf373c6e192069b60c12c9a91e6afa03
Daniel Roa <daniel.roa98@gmail.com>
2023-08-25 12:54:10 -0600

    Modified files.
    - Created files containing the basic information so that practice and understanding is easier.
    - Added exercise script.

---
+++ 2da Sesión/01-Ejercicios.py
@@ -0,0 +1,12 @@
+# 01-Ejercicios.py
+# Created by Daniel Roa on August 25, 2023
+
+""" 
+Ahora que ya saben como funcionan los comentarios y como ver los tipos de datos, veremos los siguientes ejercicios:
+
+1. Ver el tipo de dato que nos regresa la operación 192+623*1234/(45.3**23)
+
+2. La operación "1234"/54.23, ¿se puede hacer? ¿Qué les regresa?
+
+3. Programen una operación que regrese un Bool, puede ser tan elaborada como gusten.
+"""
--- 2da Sesión/01-HelloWorld.py
+++ 2da Sesión/01-HelloWorld.py
@@ -1,4 +1,4 @@
-# 01-Hello.py
+# 01-HelloWorld.py
 # Created by Daniel Roa on July 19, 2023
 
 print("Hello, World!")
